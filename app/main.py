84f6be0d36949045f5c75ea24b0d276800be1e52
Naveen <naveenfaster@gmail.com>
2022-10-09 22:29:21 +0530

    added another rest endpoint

--- app/main.py
+++ app/main.py
@@ -7,5 +7,5 @@ async def root():
     return {"message": "Hello World!"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: int):
+    return {"item_id": item_id}
